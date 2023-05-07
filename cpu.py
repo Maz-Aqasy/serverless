7f7cd86b7a080d3612a389e77c6526e009637f2d
Maziar Aqasy <89877320+Maz-Aqasy@users.noreply.github.com>
2023-05-07 10:22:16 +0330

    Update cpu.py

--- cpu.py
+++ cpu.py
@@ -1,14 +1,8 @@
 def handle(req):
-    # Perform a CPU-intensive operation (in this case, calculating prime numbers)
-    primes = []
-    for i in range(2, 100000):
-        is_prime = True
-        for j in range(2, int(i ** 0.5) + 1):
-            if i % j == 0:
-                is_prime = False
-                break
-        if is_prime:
-            primes.append(i)
+    # Perform a CPU-intensive operation
+    result = 0
+    for i in range(100000000):
+        result += i
 
-    # Return the calculated primes as a string
-    return str(primes)
+    # Return the result
+    return f"Result: {result}"
